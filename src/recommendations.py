c6f9d03d10a5664b090fdc9b832ff3c3545de382
Nikhil Garg <nikhilgarg28@gmail.com>
2012-08-15 03:59:42 +0530

    Added some more code to recommendations.py

--- src/recommendations.py
+++ src/recommendations.py
@@ -30,7 +30,7 @@ def get_common_features(b1, b2):
 
 #Returns sum of squares of difference on two lists
 def eucledian(l1, l2):
-    return sum([(l1[i] - l2[i])**2 for i in xrange(len(l1))])
+    return sum((l1[i] - l2[i])**2 for i in xrange(len(l1)))
 
 #Returns a distance-based similarity measure for bags b1 and b2
 def sim_distance(b1, b2):
@@ -45,3 +45,43 @@ def sim_distance(b1, b2):
     return similarity
 
 print sim_distance(critics['Lisa Rose'], critics['Toby'])
+
+#Returns the Pearson correlation coefficient for bags b1 and b2
+def sim_pearson(b1, b2):
+    common_features = get_common_features(b1, b2)
+    if len(common_features) == 0:
+        return 0
+
+    common_feature_vectors = [(b1[f], b2[f]) for f in common_features]
+    v1, v2 = zip(*common_feature_vectors)
+
+    sum1, sum2 = sum(v1), sum(v2)
+
+    mag1 = sum(x**2 for x in v1)
+    mag2 = sum(x**2 for x in v2)
+
+    # Dot product of vectors v1 and v2
+    dot = sum (x * y for x, y in zip(v1, v2))
+
+    N = len(common_features)
+
+    num = dot - (sum1 * sum2) / N
+
+    from math import sqrt
+    den = sqrt((mag1 - (sum1**2) / N) * (mag2 - (sum2**2) / N))
+
+    if den == 0: return 0
+
+    correlation = num / den
+    return correlation
+
+# Returns top N bags which are most similar to B from bags in candidates
+# B is a bag of features. candidates is a bag of bag of features
+def most_similar(B, candidates, num = 5, similarity = sim_pearson):
+
+    scores = [(similarity(B, candidates[c]), c) for c in candidates]
+
+    # Sort the list so the highest scores appear at the top
+    scores.sort()
+    scores.reverse()
+    return scores[:num]
